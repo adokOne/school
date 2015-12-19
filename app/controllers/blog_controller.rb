4cea90ff14462babc1dcba8a5b93afdf0e5e1d31
Chernov Alexandr <chernov.oleksandr@ki-technology.ru>
2015-12-19 16:58:29 +0200

    fixed

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -3,14 +3,14 @@ class BlogController < ApplicationController
   before_action :set_manifest
 
   def index
-    @similar = Page.limit(3).order(id: :desc)
+    @similar = Page.active.limit(3).order(id: :desc)
     @items   = Category.where(parent_id:0).all
     @top_menu = @items.inject({}){|hash, item| hash[item.seo_name] = {text: item.title}; hash }
   end
 
   def category
     @item    = Category.find_by_seo(params[:seo_name].split("/"))
-    @similar = Page.limit(3).where("category_id != ?",@item.id).order(id: :desc)
+    @similar = Page.active.limit(3).where("category_id != ?",@item.id).order(id: :desc)
     if @item.parent.present?
       only_photos = @item.parent.only_photos
     else
@@ -21,15 +21,15 @@ class BlogController < ApplicationController
 
   def item
 
-    @item = Page.find_by_seo(params[:seo_name].split("/"))
-    @similar = Page.limit(3).where("category_id != ?",@item.category_id).order(id: :desc)
+    @item = Page.active.find_by_seo(params[:seo_name].split("/"))
+    @similar = Page.active.limit(3).where("category_id != ?",@item.category_id).order(id: :desc)
   end
 
   def search
     @similar = Page.limit(3).order(id: :desc)
     @items = []
     @count = 0
-    Page.where("`title` LIKE :search AND category_id IS NOT NULL OR `anons` LIKE :search AND category_id IS NOT NULL OR `desc` LIKE :search AND category_id IS NOT NULL",{search: "%#{params[:search]}%"}).includes(:category).all.map do |page|
+    Page.active.where("`title` LIKE :search AND category_id IS NOT NULL OR `anons` LIKE :search AND category_id IS NOT NULL OR `desc` LIKE :search AND category_id IS NOT NULL",{search: "%#{params[:search]}%"}).includes(:category).all.map do |page|
       @items[page.category_id] = {category: page.category, pages:[]} if @items[page.category_id].nil?
       @items[page.category_id][:pages] << page
       @count +=1
