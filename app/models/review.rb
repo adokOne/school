872002d498b479723a33eb5eeee86081c5efd38b
Chernov Alexandr <chernov.oleksandr@ki-technology.ru>
2016-01-13 20:17:11 +0200

    fixed

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ActiveRecord::Base
 
-  validates :name,:email,:page_id,:comment presence: true
+  validates :name,:email,:page_id,:comment, presence: true
   validates :name,     length: { in: 3..50 }, format: {with:/\A^[^0-9`!@#\$%\^&*+_=]+\z/i}
   validates :email,    length: { in: 6..50 }, uniqueness: true,format:{with:/\A^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
 
