d71c62681ee87a289b500d41b99e6cf203097ed9
Chernov Alexandr <chernov.oleksandr@ki-technology.ru>
2016-03-01 11:24:16 +0200

    fixed

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -25,7 +25,7 @@ class ApplicationMailer < ActionMailer::Base
 
   def _prepare_template( template , vars)
     vars.each_pair do |key,text|
-      template.gsub!("%#{key.to_s.upcase}%",text)
+      template.to_s.gsub!("%#{key.to_s.upcase}%",text)
     end
     template
   end
