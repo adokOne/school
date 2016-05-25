afc4c00941f65ceccab81c2ed165539124e03338
Chernov Alexandr <chernov.oleksandr@ki-technology.ru>
2016-05-25 11:42:13 +0300

    fixed

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,8 +1,12 @@
 ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  :port      => 587,
-  :address   => "smtp.mandrillapp.com",
-  :user_name => "taras_sypa@ukr.net",
-  :password  => "84Ye2CzWZHnbudxe9agsSg"
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: :plain,
+    user_name: 'gandriero@gmail.com',
+    password: 'sisi100502',
+    enable_starttls_auto: true,
+    domain: 'speakclub.com.ua',
+    from: 'info@speakclub.com.ua',
 }
